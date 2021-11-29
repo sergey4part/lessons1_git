c39fca404e1aff2250cc93e6d40db7325c8a66c0
cia <cia@vodokanal.local>
2021-11-29 14:07:28 +0300

    Add

---
+++ 2.py
@@ -0,0 +1,9 @@
+def get_vat(payment, persent=20):
+	try:
+		vat = payment / 100 * persent
+		vat = round(vat, 2)
+		return "Sum VAT: {}".format(vat)
+	except TypeError:
+		return "Check"
+result = get_vat(400)
+print(result)
